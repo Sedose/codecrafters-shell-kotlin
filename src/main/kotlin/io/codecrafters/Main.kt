af6f4b1df57f933db1c6e79d887df7966a9131dc
Edhar Avuzi <Edhar.EzenvaAvuzi@kingfisher.com>
2025-05-26 21:03:26 +0300

    fr fr

--- src/main/kotlin/io/codecrafters/Main.kt
+++ src/main/kotlin/io/codecrafters/Main.kt
@@ -1,8 +1,23 @@
 package io.codecrafters
 
-fun main() {
-    // Uncomment this block to pass the first stage
-     print("$ ")
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
-    println(readln() + ": command not found")
+@SpringBootApplication
+class Main
+
+fun main(args: Array<String>) {
+  runApplication<Main>(*args)
+}
+
+@Component
+class ShellRunner : CommandLineRunner {
+  override fun run(vararg args: String) {
+    while (true) {
+      print("$ ")
+      println(readln() + ": command not found")
+    }
+  }
 }
