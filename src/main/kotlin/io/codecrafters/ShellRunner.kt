9f32a5dd0fc82fe6bed40071ef6f075a411dcb07
Edhar Avuzi <Edhar.EzenvaAvuzi@kingfisher.com>
2025-05-27 15:49:51 +0300

    fr fr

--- src/main/kotlin/io/codecrafters/ShellRunner.kt
+++ src/main/kotlin/io/codecrafters/ShellRunner.kt
@@ -3,6 +3,7 @@ package io.codecrafters
 import io.codecrafters.command.CommandHandler
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
+import java.io.IOException
 
 @Component
 class ShellRunner(
@@ -13,26 +14,29 @@ class ShellRunner(
     while (true) {
       print("$ ")
       val inputLine = readLine() ?: break
-      val trimmedLine = inputLine.trim()
-      if (trimmedLine.isEmpty()) continue
-      val tokens = trimmedLine.split(Regex("\\s+"))
+      val trimmedInput = inputLine.trim()
+      if (trimmedInput.isEmpty()) continue
+      val tokens = trimmedInput.split(Regex("\\s+"))
       val commandName = tokens.first()
-      val argumentValues = tokens.drop(1)
+      val argumentList = tokens.drop(1)
       commandHandlerMap[commandName]
-        ?.handle(argumentValues.joinToString(" "))
-        ?: executeExternalProgram(commandName, argumentValues)
+        ?.handle(argumentList.joinToString(" "))
+        ?: executeExternalProgram(commandName, argumentList)
     }
   }
 
-  private fun executeExternalProgram(commandName: String, argumentValues: List<String>) {
-    val commandLine =
-      buildList {
-        add(commandName)
-        addAll(argumentValues)
-      }
-    ProcessBuilder(commandLine)
-      .inheritIO()
-      .start()
-      .waitFor()
+  private fun executeExternalProgram(commandName: String, argumentList: List<String>) {
+    val fullCommand = buildList<String> {
+      add(commandName)
+      addAll(argumentList)
+    }
+    try {
+      val process = ProcessBuilder(fullCommand)
+        .inheritIO()
+        .start()
+      process.waitFor()
+    } catch (_: IOException) {
+      println("$commandName: not found")
+    }
   }
 }
